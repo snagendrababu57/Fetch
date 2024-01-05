493532c494242f8888033a2b935dec130e71d52b
snagendrababu57 <96361157+snagendrababu57@users.noreply.github.com>
2024-01-05 21:11:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Fetchc from './components/Fetchc';
+import Fetcha from './components/Fetchp';
+import MyComponent6 from './components/useReducer';
+import MyComponent61 from './use';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Fetchc/> */}
+      {/* <Fetcha/> */}
+      {/* <MyComponent6/> */}
+      <MyComponent61/>
+    </div>
+  );
+}
+
+export default App;
